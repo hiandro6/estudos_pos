79ef399d425a72fbffe28ae9ec4e2c130aa9d803
hiandro6 <hiandroalex6@gmail.com>
2025-06-24 09:07:51 -0300

    update models

--- 04consumoApi/models.py
+++ 04consumoApi/models.py
@@ -2,25 +2,8 @@ from pydantic import BaseModel
 from datetime import date
 from typing import List
 
-class Usuario(BaseModel):
-    username:str
-    password:str
-    data_criacao:date
-
 class Livro(BaseModel):
     titulo:str
     ano:int
     edicao:int
 
-class Emprestimo(BaseModel):
-    usuario:Usuario
-    livro:Livro
-    data_emprestimo:date
-    data_devolucao:date
-
-
-class Biblioteca(BaseModel):
-    nome:str
-    acervo: List[Livro]
-    usuarios: List[Usuario]
-    emprestimos: List[Emprestimo]
