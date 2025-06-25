3fda7143be1aafc9942877aebc66f2c49298509a
hiandro6 <hiandroalex6@gmail.com>
2025-06-25 15:58:23 -0300

    sincronizando a rota de editar

--- 04consumoApi_aux/main.py
+++ 04consumoApi_aux/main.py
@@ -9,9 +9,12 @@ if __name__ == "__main__":
         2 - Pesquisar livro por título
         3 - Cadastrar um livro
         4 - Deletar um livro
-        5 - Sair
+        5 - Editar um livro
+        6 - Sair
 
         qual operação deseja realizar?""")
+
+
         if option == "1":
             r = requests.get(f"{url}/livros")
             livros = json.loads(r.text)
@@ -21,6 +24,7 @@ if __name__ == "__main__":
                 print(f"Ano: {livro['ano']}")
                 print(f"Edição: {livro['edicao']}")
                 print("-" * 30)
+
 
         elif option == "2":
             pesquisa = input("Título do livro a ser buscado: ")
@@ -37,6 +41,7 @@ if __name__ == "__main__":
             else:
                 print("Livro não encontrado!")
         
+
         elif option == "3":
             titulo = input("digite o título do livro: ")
             ano = int(input("digite o ano do livro: "))
@@ -50,14 +55,32 @@ if __name__ == "__main__":
             print(r.status_code)
             print(r.text)
         
+
         elif option == "4":
             pesquisa = input("título do livro a ser deletado: ")
             r = requests.delete(f"{url}/livros/{pesquisa}")
             print(r.status_code)
 
+
         elif option == "5":
+            titulo_velho = input("digite o título do livro que deseja editar: ")
+            titulo = input("digite o novo título do livro: ")
+            ano = int(input("digite o novo ano do livro: "))
+            edicao = int(input("digite a nova edição do livro: "))
+            livro = {
+                "titulo": titulo,
+                "ano": ano,
+                "edicao": edicao
+            }
+            r = requests.put(f"{url}/livros/{titulo_velho}", json=livro)
+            print(r.status_code)
+            print(r.text)
+        
+
+        elif option == "6":
             print("muito obrigado por usar nosso sistema, volte sempre!")
             break
         
+
         else:
             print("opção inválida tente novamente")
